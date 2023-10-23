29c31c865138dcc2d6fbcf1390f39f675c04faf9
storycode143 <storycode143@gmail.com>
2023-10-23 12:37:45 +0530

    last commit

--- app.js
+++ app.js
@@ -1,14 +1,52 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 
+
 const app = express();
 
+app.set('view engine','ejs');
+
+app.set('views', __dirname+"/views");
+app.use(express.static(__dirname+'/public'))
+
+
+app.use(bodyParser.urlencoded({extended:false}));
+var items=[];
+
+
+
+
+
 app.get('/',function(req,res){
-    res.send('Hello');
+
+// res.re(__dirname+'/inde.html')
+// });
+    
+    const day= new Date();
+   
+    const options = {
+        weekday:"long",
+        day:'numeric',
+        month:'long'
+    }
+
+    const today= day.toLocaleString('en-US',options);
+    res.render('lists',{kindOfDay:today,newItem:items});
+    
+    
 })
 
 
+app.post('/',function(req,res){
+    var item = req.body.item;
+
+    items.push(item);
+    res.redirect('/');
+})
 
 app.listen(3000,function(){
     console.log('server started');
 });
+
+
+  
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./views/lists.ejs"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
